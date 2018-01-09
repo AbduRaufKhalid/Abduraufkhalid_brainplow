8e1d51b565b87557e4a194055e7e154f3285c130
AbduRauf Khalid <abrauf.kamboh99@gmail.com>
2018-01-09 19:50:41 +0500

    9jan
    services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HeroService } from './hero.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  constructor(private NewService: HeroService) {}
   title = 'Tour of Heroes';
+
+
+  ngOnInit() {
+    console.log(this.NewService.getHeroes());
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
+import {HeroService } from './hero.service';
+
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
@@ -17,7 +20,9 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+    providers: [ HeroService, this.MessageService ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  private static MessageService: any;
+}
